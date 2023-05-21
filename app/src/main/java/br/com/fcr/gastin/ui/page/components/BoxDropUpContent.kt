e8712f87e78bcf0c78bd93eb9b0685419e3ca0e4
francivaldo <francivaldodev@gmail.com>
2023-05-20 22:31:41 -0300

    fix

--- app/src/main/java/br/com/fcr/gastin/ui/page/components/BoxDropUpContent.kt
+++ app/src/main/java/br/com/fcr/gastin/ui/page/components/BoxDropUpContent.kt
@@ -8,61 +8,46 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun BoxDropUpContent (enable:Boolean, onDismiss:()->Unit,content:@Composable BoxScope.()->Unit){
-    val durationMillis = 500
-    AnimatedVisibility(
-        visible = enable,
-        enter = fadeIn(
-            tween(
-                durationMillis = durationMillis
-            )
-        ),
-        exit = fadeOut(
-            tween(
-                durationMillis = durationMillis
-            )
-        )
-    ) {
+
+    val expandedStates = remember { MutableTransitionState(false) }
+    expandedStates.targetState = enable
+
+    if (expandedStates.currentState || expandedStates.targetState) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .clickable(false, onClick = {})
                 .background(Color.Black.copy(0.7f))
         )
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom
-    ) {
-        if(enable) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .pointerInput(Unit) {
-                    detectTapGestures (
-                        onTap ={
-                            onDismiss()
-                        }
-                    )
-                })
-        }
-        AnimatedVisibility(
-            visible = enable,
-            enter = slideInVertically { it },
-            exit = slideOutVertically { it }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
         ) {
+            if(enable) {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onTap = {
+                                onDismiss()
+                            }
+                        )
+                    })
+            }
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
