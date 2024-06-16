6ef2520283c1c16720e0f8e985a871fb78f823cc
francivaldo costa <francivaldodev@gmail.comm>
2024-06-16 12:52:55 -0300

    version: 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.fcr.gastin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
