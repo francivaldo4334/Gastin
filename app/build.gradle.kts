bc4248aa86ae8e078df39e82bdc4fba3f875f4b2
francivaldo4334 <francivaldodev@gmail.com>
2025-05-30 10:59:00 -0300

    nomeia versão 13 para 3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "br.com.fcr.gastin"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "3.0"
+        versionCode = 13
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
